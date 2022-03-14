f63f9c5d6e812ecb34b90ce39ef588f6372228a1
Kevin Ebaugh <kevinebaugh@gmail.com>
2022-03-14 12:58:37 -0400

    Add more components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Checkin from './Checkin'
+import CheckinForm from './CheckinForm'
+import CheckinList from './CheckinList'
 
 function App() {
+  const [visits, updateVisits] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:9292/waterbowl_visits')
+      .then(response => response.json())
+      .then(visitData => {
+        updateVisits(visitData)
+      })
+  },)
+
+  function handleNewVisit(visit) {
+    fetch("http://localhost:9292/waterbowl_visits",
+      {
+        method: "POST",
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(visit)
+      })
+      .then(res => res.json())
+      .then(json => console.log(json))
+    updateVisits(
+      [...visits, visit]
+    )
+  }
+
   return (
-    <Checkin />
+    <>
+      <h1>Waterbowl Checkins</h1>
+      <CheckinForm visits={visits} handleNewVisit={handleNewVisit} />
+      <hr/>
+      <CheckinList visits={visits} />
+    </>
   );
 }
 
